8339086fe5bbb37654ccc11ba2744a7c90033c77
Daigo U <go26dev@gmail.com>
2024-02-20 22:21:29 +0900

    initial commit

---
+++ SwiftUI-Animations-Playground/SwiftUI_Animations_PlaygroundApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2024/02/20
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animations_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
